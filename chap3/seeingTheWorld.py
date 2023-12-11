5e797def16057d36c8b9c8cd1a8b5aa88773f6d8
ZuxingGit <1245766137@qq.com>
2023-12-11 15:43:01 +1030

    seeingTheWorld

---
+++ chap3/seeingTheWorld.py
@@ -0,0 +1,21 @@
+places=['Suzou', 'Adelaide','Hangzou','Hainan','Perth']
+print(places)
+
+print(sorted(places))
+print(places)
+
+print(sorted(places,reverse=True))  #here
+print(places)
+
+places.reverse()
+print(places)
+
+places.reverse()
+print(places)
+
+places.sort()
+print(places)
+
+places.sort(reverse=True)
+print(places)
+print(len(places))
