07028a76b9fc2dae36d8eefcbd55c86923a82bd5
ZuxingGit <1245766137@qq.com>
2023-12-11 21:22:37 +1030

    pizzas

---
+++ chap4/pizzas.py
@@ -0,0 +1,5 @@
+pizzas=['pizza1','pizza2','pizza3']
+for pizza in pizzas:
+    print("I like "+pizza+"!")
+    
+print(f"\nI can eat \n\t{pizzas[0]},\n\t{pizzas[1]},\n\t{pizzas[2]}\nI really love pizza!")
