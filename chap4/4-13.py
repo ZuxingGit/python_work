9e0207461865f42f75c98687d4622bdfcbccd1c2
ZuxingGit <1245766137@qq.com>
2023-12-12 18:22:08 +1030

    launch.json

---
+++ chap4/4-13.py
@@ -0,0 +1,8 @@
+foods=('bread','milk','beef','pork','avocado')
+for food in foods:
+    print(food)
+    
+# foods[1]='apple'
+foods=('noodle','apple','beef','pork','avocado')
+for food in foods:
+    print(food)
