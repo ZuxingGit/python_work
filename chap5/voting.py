ab284f59c6edf327fdd738f4602280b4dab7d35c
ZuxingGit <1245766137@qq.com>
2023-12-13 11:43:58 +1030

    voting

---
+++ chap5/voting.py
@@ -0,0 +1,7 @@
+age=17;
+if age>=18:
+    print("You are old enough to vote!")
+    print("Have u registered to vote?")
+else:
+    print("Sorry, u r too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
