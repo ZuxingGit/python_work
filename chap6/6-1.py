d965adcb1bc893fa272828055222e2f09823cabb
ZuxingGit <1245766137@qq.com>
2023-12-14 16:45:28 +1030

    TIY

---
+++ chap6/6-1.py
@@ -0,0 +1,6 @@
+person = {"first_name": "Zuxing", "last_name": "Wu", "age": 31, "city": "Adelaide"}
+
+print(person["first_name"])
+print(person["last_name"])
+print(person["age"])
+print(person["city"])
---
+++ chap6/6-2.py
@@ -0,0 +1,4 @@
+favorite_numbers={"A":1,"B":2}
+
+print(favorite_numbers["A"])
+print(favorite_numbers["B"])
