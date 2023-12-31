774d8c772b17f5d6591704a9a0e03fe79890e087
ZuxingGit <1245766137@qq.com>
2024-01-01 08:45:00 +1030

    new topic

--- projects/web_application/learning_log/learning_logs/views.py
+++ projects/web_application/learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 
 # Create your views here.
@@ -22,3 +23,41 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
+
+
+def new_topic(request):
+    """add a new topic"""
+    if request.method != "POST":
+        # no data submitted; create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+
+    # display a blank or invalid form
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    """add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        # No data submitted; create a blank form
+        form = EntryForm()
+    else:
+        # POST data submitted; process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect("learning_logs:topic", topic_id=topic_id)
+
+    # display a blank or invalid form
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
