50e296e7c0979f83f353dd61e771b171b0b23fda
ZuxingGit <1245766137@qq.com>
2024-01-01 17:25:39 +1030

    login required

--- projects/web_application/learning_log/learning_logs/views.py
+++ projects/web_application/learning_log/learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,13 +11,14 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 
+@login_required
 def topics(request):
     """show all topics"""
     topics = Topic.objects.order_by("date_added")
     context = {"topics": topics}
     return render(request, "learning_logs/topics.html", context)
 
-
+@login_required
 def topic(request, topic_id):
     """show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
@@ -24,7 +26,7 @@ def topic(request, topic_id):
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
 
-
+@login_required
 def new_topic(request):
     """add a new topic"""
     if request.method != "POST":
@@ -41,7 +43,7 @@ def new_topic(request):
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -62,7 +64,7 @@ def new_entry(request, topic_id):
     context = {"topic": topic, "form": form}
     return render(request, "learning_logs/new_entry.html", context)
 
-
+@login_required
 def edit_entry(request, entry_id):
     """edit an existing entry"""
     entry = Entry.objects.get(id=entry_id)
