016708e9d4d5a6d69d16a24889f6461a05b216fc
ZuxingGit <1245766137@qq.com>
2024-06-27 14:50:34 +0930

    IfElse

---
+++ W3Schools/IfElse.py
@@ -0,0 +1,41 @@
+# If statement:
+# a = 33
+# b = 200
+# if b > a:
+#     print("b is greater than a")
+
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# # elif a == b:
+# #   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+# One line if statement:
+# a = 200
+# b = 33
+# if a > b: print("a is greater than b")
+
+# One line if else statement:
+# a = 2
+# b = 330
+# # print("A") if a > b else print("B")
+
+# One line if else statement, with 3 conditions:
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B")
+
+a = 33
+b = 200
+if b > a:
+  pass
