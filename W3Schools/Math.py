40d515f71401292eb1e08b0b2b9ae43b2fea5dbf
ZuxingGit <1245766137@qq.com>
2024-06-29 11:49:14 +0930

    Math

---
+++ W3Schools/Math.py
@@ -0,0 +1,25 @@
+# x = pow(4, 3)
+# print(x)
+
+# x = 4**3
+# print(x)
+
+
+# import math
+# x = math.sqrt(64)
+# print(x)
+
+
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x) # returns 2
+print(y) # returns 1
+print(math.pi)
+print(math.e)
+print(math.tau)
+print(math.inf)
+print(math.nan)
+
+# https://www.w3schools.com/python/module_math.asp
